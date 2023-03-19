b9fb7c33c075bb957607dc8701cadd6b4debf381
Panos Sperdoulis <sperpan@yahoo.com>
2023-03-19 20:34:54 +0200

    Added the clear button

--- public/main.js
+++ public/main.js
@@ -1,6 +1,3 @@
-//
-
-
 //new socket obj , shorthand to io.connect()
 const socket = io();
 
@@ -16,5 +13,30 @@ socket.on('video', function(data) {
   video.src = url;
   video.autoplay = true
 
+});
+
+const commandLineInput = document.getElementById('command-line-input');
+const commandLineContainer = document.getElementById('command-line-container');
+
+const greenOutputs = [];
+
+commandLineInput.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    const command = commandLineInput.value;
+    const commandLineOutput = document.createElement('div');
+    commandLineOutput.textContent = command;
+    commandLineOutput.style.color = 'green';
+    commandLineContainer.appendChild(commandLineOutput);
+    greenOutputs.push(commandLineOutput);
+    commandLineInput.value = '';
+  }
+});
+
+const clearButton = document.querySelector('#clear-button');
+
+clearButton.addEventListener('click', () => {
+  for (let i = 0; i < greenOutputs.length; i++) {
+    greenOutputs[i].remove();
+  }
 });
 
