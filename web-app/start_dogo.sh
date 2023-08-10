73745fe4ca54e44fd5d745a4fd15354af2e3a318
sperpanakos <110525631+sperpanakos@users.noreply.github.com>
2023-08-10 17:54:13 +0300

    Add files via upload

---
+++ web-app/start_dogo.sh
@@ -0,0 +1,23 @@
+echo "123" | sshpass -p '123' ssh -tt pi@192.168.12.1 "sshpass -p '123' ssh -tt unitree@192.168.123.13 'cd UnitreecameraSDK && sudo -S <<<\"123\"  ./kill_processes.sh'"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ web-app/start_dogo2.sh
@@ -0,0 +1,10 @@
+echo "123" | sshpass -p '123' ssh -tt pi@192.168.12.1 "gst-launch-1.0 udpsrc port=8000 ! udpsink host=192.168.12.162 port=8002"
+
+
+
+
+
+
+
+
+ 
